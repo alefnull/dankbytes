b6347f077f4f7486bbb627ce9449549a3b17944c
alefnull <nullalef@gmail.com>
2025-05-11 19:21:49 -0400

    ohhh, only 'use' deps for dev window INSIDE dev window method

--- src/ui.rs
+++ src/ui.rs
@@ -4,7 +4,7 @@ use hello_egui::material_icons::icons;
 use thousands::Separable;
 
 use crate::{
-  drugs::{get_drug_list, get_rand_prices},
+  drugs::get_drug_list,
   events,
   game::{Game, GameLength},
   locations::Location,
@@ -107,6 +107,7 @@ pub fn render_window(game: &mut Game, ctx: &egui::Context) {
 // MARK: - DEV render_dev_window()
 #[cfg(debug_assertions)]
 fn render_dev_window(game: &mut Game, ctx: &egui::Context) {
+  use crate::drugs::get_rand_prices;
   egui::Window::new("Dev Tools")
     .title_bar(false)
     .movable(true)
@@ -439,3 +440,50 @@ fn render_drug_trading_table(game: &mut Game, ui: &mut egui::Ui) {
       }
     });
 }
+
+// MARK: render_new_table()
+// fn render_new_table(game: &mut Game, ui: &mut egui::Ui) {
+// fn render_new_table(game: &mut Game, ui: &mut egui::Ui) {
+//   egui_extras::TableBuilder::new(ui)
+//     .columns(Column::remainder(), 7)
+//     .striped(true)
+//     .body(|mut body| {
+//       for drug in get_drug_list().iter() {
+//         body.row(12.0, |mut row| {
+//           row.col(|ui| {
+//             ui.label(drug.to_string());
+//           });
+//           row.col(|ui| {
+//             ui.label(format!("{}", game.inventory.get_amount(*drug).unwrap_or(0)));
+//           });
+//           row.col(|ui| {
+//             ui.label(format!("${}", game.inventory.get_cost(*drug).unwrap_or(0)));
+//           });
+//           row.col(|ui| {
+//             ui.add_enabled_ui(
+//               game.cash >= game.prices[*drug as usize]
+//                 && game.inventory.get_amount(*drug).unwrap_or(0) < u32::MAX,
+//               |ui| {
+//                 let max_buy = game.cash / game.prices[*drug as usize];
+//                 ui.horizontal(|ui| {
+//                   DragValue::new(&mut game.buy_amts[*drug as usize])
+//                     .range(0..=max_buy)
+//                     .ui(ui);
+//                   if ui.button("Buy").clicked() {
+//                     game.buy(*drug, 1);
+//                   }
+//                 });
+//               },
+//             );
+//           });
+//           row.col(|ui| {
+//             ui.add_enabled_ui(game.inventory.get_amount(*drug).unwrap_or(0) > 0, |ui| {
+//               if ui.button("Sell").clicked() {
+//                 game.sell(*drug, 1);
+//               }
+//             });
+//           });
+//         });
+//       }
+//     });
+// }
