0d458d9bb2314f773c8f3037b39cbc09717ffb94
alefnull <nullalef@gmail.com>
2025-05-12 17:03:48 -0400

    remove file version from windows exe because who cares

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ fn main() {
     let mut res = winres::WindowsResource::new();
     res
       .set_icon("./assets/leaf.ico")
-      .set("FileVersion", "0.0.1")
       .set("ProductName", "Dank Bytes");
 
     if let Err(e) = res.compile() {
